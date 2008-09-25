2d05be46824b2bbce21ca7dfd8f48e2f43163eda
Adam Smith <ajs17@cornell.edu>
2008-09-25 20:13:41 +0000

    git-svn-id: https://svn.library.cornell.edu/cul_common@8 fa0dc9c2-24ac-6fec-b018-c4f86088f1c3

--- trunk/modules/cul_common/authenticate/index.php
+++ trunk/modules/cul_common/authenticate/index.php
@@ -2,10 +2,12 @@
 $netID = getenv('REMOTE_USER');
 if (isset($netID) && $netID != '') {
     setcookie('netid', $netID, 0, '/', '.cornell.edu');
-    setcookie('primary_affiliation',  exec('java SimpleQuery uid=' . $netID . '
-eduPersonPrimaryAffiliation'), 0, '/', '.cornell.edu');
+    #setcookie('primary_affiliation',  exec('java SimpleQuery uid=' . $netID . ' eduPersonPrimaryAffiliation'), 0, '/', '.cornell.edu');
+} else {
+  echo 'no net id';
 }
-header('Location: /node/' . $_COOKIE['destination_node']);
+echo '\n<br />destination: ' . $_GET['destination'];
+#header('Location: ' . $_GET['destination']);
 exit();
 ?>
 
