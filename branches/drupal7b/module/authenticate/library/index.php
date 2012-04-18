81de130ade50da8353fb67a3480a3abee11db87c
James Reidy <jgr25@cornell.edu>
2012-04-18 18:48:08 +0000

    debugging
    git-svn-id: https://svn.library.cornell.edu/cul_common@125 fa0dc9c2-24ac-6fec-b018-c4f86088f1c3

--- branches/drupal7b/module/authenticate/library/index.php
+++ branches/drupal7b/module/authenticate/library/index.php
@@ -4,6 +4,10 @@ $absolutepath=realpath($localPath);
 // a fix for Windows slashes
 $absolutepath=str_replace("\\","/",$absolutepath);
 $docroot=substr($absolutepath,0,strpos($absolutepath,$localpath));
+echo "local: $localpath" . PHP_EOL
+echo "absolute: $absolutepath" . PHP_EOL
+echo "docroot: $docroot" . PHP_EOL
+
 include $docroot."/sites/all/modules/custom/cul_common/authenticate/index.php";
 
 //include DRUPAL_ROOT . '/' . realpath(dirname(__FILE__) . '/' . '../index.php');
