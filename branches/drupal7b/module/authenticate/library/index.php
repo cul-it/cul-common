e5733cd313e6cd8a7ae500a5dbf7eed766e05d6e
James Reidy <jgr25@cornell.edu>
2012-04-18 18:43:01 +0000

    DRUPAL_ROOT is not defined. Adjust the finding of settings.php
    git-svn-id: https://svn.library.cornell.edu/cul_common@124 fa0dc9c2-24ac-6fec-b018-c4f86088f1c3

--- branches/drupal7b/module/authenticate/library/index.php
+++ branches/drupal7b/module/authenticate/library/index.php
@@ -1,4 +1,11 @@
 <?php
-include DRUPAL_ROOT . '/' . realpath(dirname(__FILE__) . '/' . '../index.php');
+$localpath=getenv("SCRIPT_NAME");
+$absolutepath=realpath($localPath);
+// a fix for Windows slashes
+$absolutepath=str_replace("\\","/",$absolutepath);
+$docroot=substr($absolutepath,0,strpos($absolutepath,$localpath));
+include $docroot."/sites/all/modules/custom/cul_common/authenticate/index.php";
+
+//include DRUPAL_ROOT . '/' . realpath(dirname(__FILE__) . '/' . '../index.php');
 ?>
 
