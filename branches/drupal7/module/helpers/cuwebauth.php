f680a2dbfe85b5103226fdbb3eaae73cd2115a92
James Reidy <jgr25@cornell.edu>
2012-04-18 14:25:00 +0000

    =>
    git-svn-id: https://svn.library.cornell.edu/cul_common@112 fa0dc9c2-24ac-6fec-b018-c4f86088f1c3

--- branches/drupal7/module/helpers/cuwebauth.php
+++ branches/drupal7/module/helpers/cuwebauth.php
@@ -113,7 +113,7 @@ function cuwebauth_logout_from_url() {
 
 function get_cuwebauth($node) {
     return db_query('SELECT nid FROM {cuwebauth} where nid = :nid', 
-    array(':nid' -> $node->nid)
+    array(':nid' => $node->nid)
     )->fetchObject();
 }
 
